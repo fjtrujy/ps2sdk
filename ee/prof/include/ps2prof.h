0d5df257de86d7310211578befa7a9d8fe43ace9
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-01-14 00:46:11 +0100

    First gprof try

---
+++ ee/prof/include/ps2prof.h
@@ -0,0 +1,24 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+#ifndef __PS2PROF_H__
+#define __PS2PROF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void gprof_cleanup();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* __PS2PROF_H__ */
